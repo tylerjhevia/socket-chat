2c406639e230fdf8023ea6188dfe0299abf0b9f4
Tyler Hevia <tylerhevia@gmail.com>
2017-10-29 19:51:18 -0600

    fix commit

--- index.js
+++ index.js
@@ -28,31 +28,3 @@ io.on('connection', function(socket) {
     io.emit('typing message', messsage);
   });
 });
-
-// const app = require('express')();
-// const http = require('http').Server(app);
-// const io = require('socket.io')(http);
-
-// app.get('/', (request, response) => {
-//   response.sendFile(__dirname + '/index.html');
-// });
-
-// io.on('connection', socket => {
-//   io.emit('logged in', 'A new user signed on.')
-
-//   socket.on('chat message', msg => {
-//     io.emit('chat message', msg);
-//   });
-
-//   socket.on('disconnect', () => {
-//     io.emit('logged out', 'A user logged out.')
-//   })
-
-//   socket.on('typing', msg => {
-//     io.emit('is typing', msg)
-//   })
-// });
-
-// http.listen(3000, () => {
-//   console.log('listening on *:3000');
-// });
